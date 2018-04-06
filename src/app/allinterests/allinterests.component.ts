64728bc3747eebdddbe697493d73eeb8a7b9cbd0
Alex J.Y <13817282645@163.com>
2018-04-06 14:54:20 +0800

    mansory

--- src/app/allinterests/allinterests.component.ts
+++ src/app/allinterests/allinterests.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MyInterestService } from '../my-interest.service';
 import { AuthService } from '../auth.service';
 import { GlobalEventsManager } from '../GlobalEventsManager';
+import { NgxMasonryOptions } from '../ngx-masonry/ngx-masonry-options.interface';
 
 @Component({
   selector: 'app-allinterests',
@@ -9,6 +10,14 @@ import { GlobalEventsManager } from '../GlobalEventsManager';
   styleUrls: ['./allinterests.component.css']
 })
 export class AllinterestsComponent implements OnInit {
+
+    public masonryOptions: NgxMasonryOptions = {
+      transitionDuration: '0.2s',
+      gutter: 3,
+      resize: true,
+      initLayout: true,
+      fitWidth: true
+    };
 
   constructor(private myinterests:MyInterestService,
               private authservice:AuthService,
